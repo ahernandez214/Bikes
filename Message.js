15299cf6047ed00f67c329bedff06099a5be826b
ahernandez214 <142252007+ahernandez214@users.noreply.github.com>
2024-07-17 00:03:16 -0400

    Update Message.js

--- Message.js
+++ Message.js
@@ -1,7 +1,3 @@
-import React from "react";
-
-const Message = ({ text }) => {
+export default function Message({ text }) {
   return <p className="message">{text}</p>;
-};
-
-export default Message;
+}
